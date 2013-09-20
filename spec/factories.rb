b24b21d302360314dd29cb1660e3bdefa9b649c6
Yves Accad <yaccad@gmail.com>
2013-09-20 14:10:24 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Yves Accad"
+    email    "yaccad@gmail.com"
+    password "cornerstone"
+    password_confirmation "cornerstone"
+  end
+end
